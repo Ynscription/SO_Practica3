0c95c8eb08fbd3e17c647dab070109b1e367bd79
Ynscription <ynscription@gmail.com>
2016-11-14 14:43:48 +0100

    Added tick function to interface

--- src/Pr03/sched_prio.c
+++ src/Pr03/sched_prio.c
@@ -140,6 +140,6 @@ sched_class_t prio_sched= {
     //.task_free=task_free_prio,
     .pick_next_task=pick_next_task_prio,
     .enqueue_task=enqueue_task_prio,
-    //.task_tick=task_tick_prio,
+    .task_tick=task_tick_prio,
     .steal_task=steal_task_prio
 };
