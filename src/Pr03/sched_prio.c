6781f4af970a44201db6c09e9265b1ac639b6420
Ynscription <ynscription@gmail.com>
2016-11-14 14:40:10 +0100

    Fixed sched.c, fixed sched_prio.c

--- src/Pr03/sched_prio.c
+++ src/Pr03/sched_prio.c
@@ -1,8 +1,8 @@
 #include "sched.h"
 
-struct prio_data {
+/*struct prio_data {
     int remaining_ticks_slice;
-};
+};*/
 
 /*static int task_new_prio(task_t* t)
 {
@@ -27,12 +27,16 @@ static void task_free_prio(task_t* t)
 
 static task_t* pick_next_task_prio(runqueue_t* rq,int cpu)
 {
+	if (!rq) {
+		printf ("Big time error!\n");
+		return NULL;
+	}
     task_t* t=head_slist(&rq->tasks);
 
     /* Current is not on the rq -> let's remove it */
-    if (t)
+    if (t){
         remove_slist(&rq->tasks,t);
-
+	}
     return t;
 }
 
@@ -40,32 +44,54 @@ static int compare_tasks_prio (void *t1,void *t2)
 {
     task_t* tsk1=(task_t*)t1;
     task_t* tsk2=(task_t*)t2;
-    return tsk2->prio-tsk1->prio;
+	if (tsk1 && tsk2) {
+		return tsk1->prio-tsk2->prio;
+	}
+    else {
+		printf ("Big Time error! \n");
+		return 0;
+	}
 }
 
 static void enqueue_task_prio(task_t* t,int cpu, int runnable)
 {
     runqueue_t* rq=get_runqueue_cpu(cpu);
-
+	if (!rq) {
+		printf("Big time error!\n");
+		return;
+	}
+	if (!t) {
+		printf ("Big time error!\n");
+		return;
+	}
 	//if the task is already on the run queue or its the idle task, do nothing
-    if (t->on_rq || is_idle_task(t))
+    if (t->on_rq || is_idle_task(t)){
         return;
+	}
 	//
     if (t->flags & TF_INSERT_FRONT) {
         //Clear flag
         t->flags&=~TF_INSERT_FRONT;
         sorted_insert_slist_front(&rq->tasks, t, 1, compare_tasks_prio);  //Push task
-    } else
+    } else {
         sorted_insert_slist(&rq->tasks, t, 1, compare_tasks_prio);  //Push task
-
+	}
 
 
     /* If the task was not runnable before, update the number of runnable tasks in the rq*/
     if (!runnable) {
         task_t* current=rq->cur_task;
+		if (!current) {
+			printf ("Big Time error! \n");
+			return;
+		}
+
 
         /* Trigger a preemption if this task has a higher priority than current */
-        if (preemptive_scheduler && (!is_idle_task(current)) && (t->prio > current->prio)) {
+		/*if (is_idle_task(current)) {
+			return;
+		}*/
+        if (preemptive_scheduler && (t->prio < current->prio)) {
             rq->need_resched=TRUE;
             current->flags|=TF_INSERT_FRONT; /* To avoid unfair situations in the event
                                                 another task with the same priority wakes up as well*/
@@ -74,37 +100,37 @@ static void enqueue_task_prio(task_t* t,int cpu, int runnable)
 }
 
 
-static void task_tick_prio(runqueue_t* rq,int cpu)
+/*static void task_tick_prio(runqueue_t* rq,int cpu)
 {
-    task_t* current=rq->cur_task;
-    task_t next=rq->idle_task;
+    task_t* current = rq->cur_task;
+    task_t* next = head_slist(&rq->tasks);
     //struct rr_data* cs_data=(struct rr_data*) current->tcs_data;
 
     if (is_idle_task(current))
         return;
 
-    /*if(current->remaining_ticks_slice<=0){
+    if(current->remaining_ticks_slice<=0){
         rq->need_resched=TRUE; //force resched
     }
 
     if(current->runnable_ticks_left==1){
         rq->nr_runnable--; //la tarea o bien está saliendo o se va a dormir justo ahora
-    }*/
-
-    if(current->prio < next.prio){
-        rq->need_resched=TRUE; //force resched
     }
+	if (current && next) {
+    	if(current->prio > next->prio && preemptive_scheduler){
+        	rq->need_resched=TRUE; //force resched
+    	}
+	}
 
-}
+}*/
 
 static task_t* steal_task_prio(runqueue_t* rq,int cpu)
 {
     task_t* t=tail_slist(&rq->tasks);
-
     if (t) {
         remove_slist(&rq->tasks,t);
         t->on_rq=FALSE;
-        rq->nr_runnable--;
+        //rq->nr_runnable--;
     }
     return t;
 }
@@ -114,6 +140,6 @@ sched_class_t prio_sched= {
     //.task_free=task_free_prio,
     .pick_next_task=pick_next_task_prio,
     .enqueue_task=enqueue_task_prio,
-    .task_tick=task_tick_prio,
+    //.task_tick=task_tick_prio,
     .steal_task=steal_task_prio
 };
